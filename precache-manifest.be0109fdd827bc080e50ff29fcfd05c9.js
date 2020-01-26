1068c5ba01254c326e660cecb2123d16f063913e
ErickJoestar <erickborquez@gmail.com>
2020-01-26 10:45:12 -0600

    Updates

---
+++ precache-manifest.be0109fdd827bc080e50ff29fcfd05c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5af2b57b1301bd739babd5a8a6a5d30c",
+    "url": "/react-memorama/index.html"
+  },
+  {
+    "revision": "07ef0501b4ee865a2de4",
+    "url": "/react-memorama/static/css/main.0cd6ff66.chunk.css"
+  },
+  {
+    "revision": "5e9215d19db1a48ea989",
+    "url": "/react-memorama/static/js/2.e13b527e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-memorama/static/js/2.e13b527e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "07ef0501b4ee865a2de4",
+    "url": "/react-memorama/static/js/main.b0b929f5.chunk.js"
+  },
+  {
+    "revision": "e634b5db5c978c81d01c",
+    "url": "/react-memorama/static/js/runtime-main.e6fef573.js"
+  }
+]);
